4cf7e6ceac6bddc7e7229ec4afe8b6d319fcd2ce
Surya Vamsi Kalidindi <srisuryakalidindi28@gmail.com>
2025-06-21 18:42:47 +0530

    added healthcheck

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ app.use(express.json());
 
 app.use("/api", authRoutes);
 app.use("/api", resumeRoutes);
+app.get("/health", (req, res) => {
+  res.status(200).json({ status: "ok", timestamp: new Date().toISOString() });
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log("Server running on http://localhost:3000"));
