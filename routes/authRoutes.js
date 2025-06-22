adb3ec1fe8e5f74a79c59ea371027d275c7e60fa
Surya Vamsi Kalidindi <srisuryakalidindi28@gmail.com>
2025-06-22 12:49:12 +0530

    added individual options

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,6 +4,8 @@ const { register, login } = require("../controllers/authController");
 const cors = require("cors");
 const { corsOptions } = require("../constants/cors");
 
+router.options("/register", cors(corsOptions));
+router.options("/login", cors(corsOptions));
 router.post("/register", cors(corsOptions), register);
 router.post("/login", cors(corsOptions), login);
 
